c2b6490d6744bb93b4e73df8df57dd4d41e4ed72
Anna Karutina <anna.karutina@khk.ee>
2021-11-15 11:18:18 +0200

    Created Person class getter and setter

--- Person.js
+++ Person.js
@@ -3,9 +3,19 @@ class Person{
     // f-on for object creation
     constructor(name) {
        this.name = name;
+       this.age;
     }
     // class functions
     greeting(){
         console.log('Hi, i am ' + this.name + '!')
+    }
+
+    // setter and getter
+    setAge(age){
+        this.age = age;
+    }
+
+    getAge(){
+        return this.age;
     }
 }
--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 let person1 = new Person('Bob')
 console.log(person1)
 person1.greeting()
+person1.setAge(25)
+console.log(person1.getAge())
